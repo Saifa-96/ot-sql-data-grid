79a3a30f9864e22ce4a064cd69b0543e20f6579f
Saifa-96 <450412958@qq.com>
2025-04-22 10:43:24 +0800

    feat: upgrade parser

--- packages/operational-transformation/src/parser/ast.test.ts
+++ packages/operational-transformation/src/parser/ast.test.ts
@@ -75,4 +75,55 @@ describe("ast", () => {
       );
     }
   });
+
+  test("sql2String", () => {
+    const sql = `
+      SELECT emp_name, incentive 
+      FROM (
+        VALUES 
+          (1, 'Alice', 5000, 5000 * 0.1),
+          (2, 'Bob', 6000, 6000 * 0.15),
+          (3, 'Charlie', 7000, 7000 * 0.2)
+       ) AS my_data(emp_id, emp_name, base_salary, incentive)
+      WHERE incentive > 500;
+    `;
+    const result = new Parser(sql).safeParse();
+    expect(result.type).toBe("success");
+    if (result.type === "success") {
+      const sqlStr = methods.sql2String(result.sql);
+      expect(sqlStr).toBe(
+        `SELECT emp_name, incentive FROM (VALUES (1,'Alice',5000,(5000 * 0.1)),(2,'Bob',6000,(6000 * 0.15)),(3,'Charlie',7000,(7000 * 0.2))) AS my_data(emp_id,emp_name,base_salary,incentive) WHERE incentive > 500;`
+      );
+    }
+
+    const sql2 = `
+    SELECT 'a' AS col1, 1 AS col2
+    UNION ALL
+    SELECT 'b', 2
+    UNION ALL
+    SELECT 'c', 3;
+    `;
+    const result1 = new Parser(sql2).safeParse();
+    const sqlObj: methods.SelectStatement = {
+      type: "select",
+      columns: [
+        { expr: { type: "String", value: "a" }, alias: "col1" },
+        { expr: { type: "Integer", value: 1 }, alias: "col2" },
+      ],
+      unionAll: [
+        [
+          { type: "String", value: "b" },
+          { type: "Integer", value: 2 },
+        ],
+        [
+          { type: "String", value: "c" },
+          { type: "Integer", value: 3 },
+        ],
+      ],
+    };
+    expect(result1).toEqual({
+      type: "success",
+      sql: sqlObj,
+    });
+  });
 });
